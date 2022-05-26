32a7702c8417f2496f3f36d47630697cc147ac8c
goobersoft <support@goobersoft.com>
2022-05-25 21:31:00 -0700

    2022-05-25

--- gb/modules/debug.c
+++ gb/modules/debug.c
@@ -42,21 +42,38 @@ void debug_draw_pre() {
 void debug_draw_pre() {
   //graph_cls( gb_graph() );
   graph_set_color( gb_graph(), 0 );
-  graph_draw_rect_spray( gb_graph(), 0, 0, 400, 240, 1 );
+  //graph_draw_rect_spray( gb_graph(), 0, 0, 400, 240, 4 );
   
-  static int x  = 0;
-  static int x2 = 0;
-  static int c  = 0;
-  static int cs = 0;
+  static int x   = 0;
+  static int x2  = 0;
+  static int c   = 0;
+  static int cs  = 0;
+  static int cls = 0;
   x = wrap(x+5+prob(50),0,1000);
-  x2 = wrap(x2+1+prob(50),0,1000);
+  x2 = wrap(x2+2+prob(50),0,1000);
   cs += 1;
   if (cs==1) {
     cs = 0;
     c = wrap(c+1,1,4);
   }
+  /*
   graph_set_color( gb_graph(), color(0,3,0) );
-  graph_draw_circle_dots( gb_graph(), 200, 120, 60 + frac(sine(x2),1000,60), 3, x );
+  int xx = frac(sine(x2),1000,20);
+  graph_draw_circle_dots( gb_graph(), 200, 160 - xx, 20 + xx, 5, x );
+  */
+  static int x1 = rnd(0,400);
+  static int x2 = rnd(0,400);
+  if (prob(1)) {
+    graph_set_color( gb_graph(), color_random() );
+    graph_draw_line( gb_graph(), rnd(0,400), rnd(0,240), rnd(0,400), rnd(0,240) );
+  }
+
+  /*
+  cls = (cls+3) % 240;
+  graph_draw_replace( gb_graph(),0,cls,400,3,color(0,1,0),color(0,0,0));
+  graph_draw_replace( gb_graph(),0,cls,400,3,color(0,2,0),color(0,1,0));
+  graph_draw_replace( gb_graph(),0,cls,400,3,color(0,3,0),color(0,2,0));
+  */
 }
 
 void debug_draw_post() {
