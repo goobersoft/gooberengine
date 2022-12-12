a38cd15d1dc0142ae2aa923f8c01ffc01c415d23
support@goobersoft.com <support@goobersoft.com>
2022-12-12 01:08:44 -0700

    2022-12-12: Build Script changes

---
+++ ma.c
@@ -0,0 +1,86 @@
+
+///////////////
+// libraries //
+///////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+
+#include </usr/include/SDL2/SDL.h>
+#include </usr/include/SDL2/SDL_mixer.h>
+#include </usr/include/SDL2/SDL_image.h>
+#include </usr/include/SDL2/SDL_net.h>
+
+///////////////////////////////////////
+// include the GB master module      //
+// as well as the game being loaded. //
+///////////////////////////////////////
+
+#include "gb/gb.c"
+#include "ma/ma.c"
+
+////////////
+// events //
+////////////
+
+void init() {
+  // set prng seed
+  srand        (time(NULL));
+  // initialize SDL2
+  SDL_Init     (SDL_INIT_EVERYTHING);
+  IMG_Init     (IMG_INIT_PNG);
+  Mix_Init     (MIX_INIT_OGG|MIX_INIT_MOD);
+  SDLNet_Init  ();
+
+  // open the audio device.
+  Mix_OpenAudio( 44100, MIX_DEFAULT_FORMAT, 1, 4096 );
+
+  gb_init();
+  ma_init();
+}
+
+void load() {
+  gb_load();
+  ma_load();
+}
+
+void start() {
+  gb_start();
+  ma_start();
+}
+
+void update() {
+  gb_update();
+  ma_update();
+}
+
+void draw() {
+  gb_draw_pre();
+  ma_draw();
+  gb_draw_post();
+}
+
+void quit() {
+  gb_quit();
+  ma_quit();
+  SDL_Quit();
+}
+
+//////////
+// main //
+//////////
+
+int main(int c, char ** v) {
+  init();
+  load();
+  start();
+  while (gb_running()) {
+    update();
+    draw();
+  }
+  quit();
+
+  return 0;
+}
